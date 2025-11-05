f7330e212b308993d9c99c69de9f032ba218236f
Konrad Goldenbaum <konradgoldenbaum@outlook.de>
2025-11-06 00:15:10 +0100

    gpu support added

--- replicate_MVF/runs/train.py
+++ replicate_MVF/runs/train.py
@@ -215,10 +215,9 @@ def eval_epoch(val_loader, model, val_meter, cur_epoch, cfg):
 
     for cur_iter, (inputs, masks, labels) in enumerate(val_loader):
         if misc.get_num_gpus(cfg):
-            # Transferthe data to the current GPU device.
-            if cfg.AUGMENTATION.USE_GPU:
-                inputs = tu.tensor2cuda(inputs)
+            inputs = tu.tensor2cuda(inputs)
             labels = tu.tensor2cuda(labels)
+            masks = tu.tensor2cuda(masks)
 
         preds, logits = model(inputs, masks)
         if cfg.PRETRAIN.ENABLE and (cfg.PRETRAIN.GENERATOR == 'MoSIGenerator'):
