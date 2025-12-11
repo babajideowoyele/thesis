2719c5069738b1dc1506c5e198e6bc16c9d80b74
Konrad Goldenbaum <konradgoldenbaum@outlook.de>
2025-12-11 19:01:00 +0100

    minor appendages

--- replicate_MVF/train_the_fucker.py
+++ replicate_MVF/train_the_fucker.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     cfg = Config(load=True)
     world_size = cfg.NUM_GPUS
     if world_size <= 1:
-        train(cfg, world_size=world_size)
+        train(0, cfg, world_size=world_size)
     else:
         from torch.multiprocessing.spawn import spawn
         spawn(train, args=(cfg, world_size), nprocs=world_size)
